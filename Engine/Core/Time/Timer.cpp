b25b24bc50eb5054d874955f14a128d62bac1ced
Pham Thanh Quang <quang20201@gmail.com>
2026-04-01 22:46:01 +0900

    update timer to chrono for cross-platform support

--- Engine/Core/Time/Timer.cpp
+++ Engine/Core/Time/Timer.cpp
@@ -1,19 +1,16 @@
 #include"Core/Time/Timer.h"
 
 namespace VIEngine {
-    Timer::Timer() : mSecondsPerCount(0.0f), mDeltaTime(-1.0f), mBaseTime(0), mPausedTime(0), 
-        mPreviousTime(0), mCurrentTime(0), mStopTime(0), mIsStopped(false)
+    Timer::Timer() : mDeltaTime(-1.0f), mBaseTime(), mPausedTime(), 
+        mStopTime(), mCurrentTime(), mPreviousTime(), mIsStopped(false)
     {
-        int64_t countsPerSec;
-        QueryPerformanceFrequency((LARGE_INTEGER*)&countsPerSec);
-        mSecondsPerCount = 1.0 / (double)countsPerSec;
     }
 
     Timer::~Timer() {
         
     }
 
-    float Timer::GetTotalTimeSeconds() {
+    float Timer::GetTotalTime() {
         // 停止中の場合、停止した瞬間から現在までの経過時間はカウントしません。
         // また、以前に一時停止があった場合、(mStopTime - mBaseTime) には
         // その停止時間が含まれてしまうため、これをカウントから除外する必要があります。
@@ -22,7 +19,7 @@ namespace VIEngine {
         // ----*---------------*-----------------------*------------*------> 時間 (Time)
         //  mBaseTime       mStopTime             startTime    mCurrentTime
         if (mIsStopped) {
-            return (float)((mStopTime - mPausedTime) - mBaseTime) * mSecondsPerCount;
+            return std::chrono::duration<float>((mStopTime.time_since_epoch() - mPausedTime.time_since_epoch()) - mBaseTime.time_since_epoch()).count();
         }
 
         // (mCurrTime - mBaseTime) には停止時間が含まれていますが、
@@ -34,62 +31,52 @@ namespace VIEngine {
         //                     |<-- 停止時間 (Paused Time) -->|
         // ----*---------------*-----------------*------------*------> 時間 (Time)
         //  mBaseTime       mStopTime       startTime     mCurrentTime
-        return (float)((mCurrentTime - mPausedTime) - mBaseTime) * mSecondsPerCount;
+        return std::chrono::duration<float>((mCurrentTime.time_since_epoch() - mPausedTime.time_since_epoch()) - mBaseTime.time_since_epoch()).count();
     }
 
     void Timer::Reset() {
-        int64_t currentTime;
-        QueryPerformanceFrequency((LARGE_INTEGER*)&currentTime);
-
-        mBaseTime = currentTime;
-        mPreviousTime = currentTime;
-        mStopTime = 0;
+        mBaseTime = Clock::now();
+        mPreviousTime = Clock::now();
+        mStopTime = {};
         mIsStopped = false;
     }
 
     void Timer::Start() {
-        int64_t currentTime;
-        QueryPerformanceFrequency((LARGE_INTEGER*)&currentTime);
-        
+        if (!mIsStopped) return;
         // 停止（Stop）と開始（Start）のペアの間に経過した時間を累積します。
         //
         //                     |<------- d (停止期間) ------->|
         // ----*---------------*-----------------------------*------------> 時間 (time)
         //  mBaseTime       mStopTime                     currentTime
-        if (mIsStopped) {
-            mPausedTime += currentTime - mStopTime;
-            mPreviousTime = currentTime;
-            mStopTime = 0;
-            mIsStopped = false;
-        }
+        mCurrentTime = Clock::now();
+        mPausedTime += mCurrentTime - mStopTime;
+        mPreviousTime = mCurrentTime;
+        mStopTime = {};
+        mIsStopped = false;
     }
 
     void Timer::Stop() {
         if (mIsStopped) return;
 
-        QueryPerformanceFrequency((LARGE_INTEGER*)&mStopTime);
+        mStopTime = Clock::now();
         mIsStopped = true;
     }
 
     void Timer::Update() {
-        if(mIsStopped)
-        {
+        if(mIsStopped) {
             mDeltaTime = 0.0;
             return;
         }
 
-        int64_t currentTime;
-        QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
-        mCurrentTime = currentTime;
+        mCurrentTime = Clock::now();
 
-        mDeltaTime = (mCurrentTime - mPreviousTime) * mSecondsPerCount;
+        mDeltaTime = (mCurrentTime - mPreviousTime).count();
         mPreviousTime = mCurrentTime;
 
         // 非負（0以上）であることを保証します。DXSDKのCDXUTTimeの記述によると、
         // プロセッサが省電力モードに移行したり、別のプロセッサへスレッドが
         // スケジューリングされたりした場合、mDeltaTime が負の値になることがあります。
-        if(mDeltaTime < 0.0)
-        {
+        if(mDeltaTime < 0.0) {
             mDeltaTime = 0.0;
         }
     }
--- Engine/Core/Time/Timer.h
+++ Engine/Core/Time/Timer.h
@@ -1,32 +1,34 @@
 #pragma once
 
-// 参照: https://github.com/d3dcoder/d3d12book/blob/master/Common/GameTimer.h
-
 #include"Core/pch.h"
+#include<chrono>
 
 namespace VIEngine {
+    using Clock = std::chrono::high_resolution_clock;
+    using TimePoint = std::chrono::time_point<Clock>;
+
     class VI_API Timer {
     public:
         Timer();
         ~Timer();
         
-        // Reset() が呼ばれてからの経過時間の合計を返します。停止中の時間は含まれません。
-        float GetTotalTimeSeconds();
+        // Reset() が呼ばれてからの秒単位の経過時間の合計を返します。停止中の時間は含まれません。
+        float GetTotalTime();
         
-        VI_FORCE_INLINE float GetDeltaTimeSeconds() { return mDeltaTime; }
+        // 前回のフレームからの秒単位の経過時間を返します。
+        VI_FORCE_INLINE double GetDeltaTime() { return mDeltaTime; }
 
         void Reset();
         void Start();
         void Stop();
         void Update();
     private:
-        double mSecondsPerCount;
         double mDeltaTime;
-        int64_t mBaseTime;
-        int64_t mPausedTime;
-        int64_t mStopTime;
-        int64_t mPreviousTime;
-        int64_t mCurrentTime;
+        TimePoint mBaseTime;
+        TimePoint mPausedTime;
+        TimePoint mStopTime;
+        TimePoint mCurrentTime;
+        TimePoint mPreviousTime;
         bool mIsStopped;
     };
 }
